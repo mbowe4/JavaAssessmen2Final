824f987f4d1da3f6770a146ee6f28aefbe18f666
mbowe4 <mlbowe@gmail.com>
2018-03-05 11:27:57 -0500

    first commit

--- src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
+++ src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
@@ -1,4 +1,31 @@
 package com.zipcodewilmington.assessment2.part4;
 
-public class Geometry {
+public class Geometry implements Rectangle, Circle {
+    int height;
+    int width;
+    int radius;
+
+    public Geometry(int height, int width, int radius) {
+        this.height = height;
+        this.width = width;
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(int radius) {
+        return 0;
+    }
+
+    @Override
+    public int area(int height, int width) {
+        return 0;
+    }
+
+    public int getArea(int height, int width) {
+        return height * width;
+    }
+
+    public double getArea(int radius) {
+        return 3.14 * (radius * radius);
+    }
 }

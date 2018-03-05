d37113421e32c93581316c1fdab9a5ad5753459b
mbowe4 <mlbowe@gmail.com>
2018-03-05 15:19:56 -0500

    second commit

--- src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
+++ src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
@@ -1,8 +1,40 @@
 package com.zipcodewilmington.assessment2.part6;
 
+import java.util.Arrays;
+
 public class LoveLetter {
+private static final String letters = "ABCDEFGHIJKLMNOP";
+int distance;
+int locationInAlphabet;
+
+    public static String reverse(String stringToCheck) {
+        StringBuilder revOrder = new StringBuilder(stringToCheck);
+        return revOrder.reverse().toString();
+    }
+
+    public boolean isPalindromic(String stringToCheck) {
+        return stringToCheck.equals(reverse(stringToCheck));
+    }
+
+    public int getAlphabetIndex(char char1) {
+        return locationInAlphabet = letters.indexOf(char1);
+    }
 
     public Integer[] mystery(String[] input) {
-        return null;
+        Integer[] resultArr = new Integer[input.length];
+        for(int i = 0; i < input.length; i++) {
+            if (!isPalindromic(input[i])) {
+                int halfWayPoint = (int) Math.floor(input[i].length() / 2);
+                for(int j = halfWayPoint; j < input[i].length(); j++) {
+                    distance = (input[i].charAt(j) - getAlphabetIndex(input[i].charAt(j)));
+                    //resultArr[i] = distance;
+                }
+
+
+            }
+            resultArr[i] = distance;
+
+        }
+        return resultArr;
     }
 }
